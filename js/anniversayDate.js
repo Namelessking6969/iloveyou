8ef00fc463125975dc09a682c3c897b15d51a774
Namelessking6969 <112624206+Namelessking6969@users.noreply.github.com>
2023-01-06 18:16:11 -0500

    updated

---
+++ js/anniversayDate.js
@@ -0,0 +1,7 @@
+let anniversayDate = new Date();
+anniversayDate.setFullYear(2020, 5, 25);
+let today = new Date();
+let yearsTogether = today.getFullYear() - anniversayDate.getFullYear();
+-alert(We, have, been, married);
+for ($; { yearsTogether }; years!)
+    ;
